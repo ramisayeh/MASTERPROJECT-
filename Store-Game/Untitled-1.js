89ba8f66d3ab4deb8f619e3f2d8abc6e53b8ab6e
ramisayeh <rami6sayeh@gmail.com>
2020-10-24 09:21:12 +0100

    done

---
+++ Store-Game/Untitled-1.js
@@ -0,0 +1,5 @@
+$('#' + 'c'+i).append(`<img class="allimg" src="${gamess[i].imgurl}"  />`);
+        $('#' + 'c'+i).append(`<h5 class="card-title"> ${gamess[i].title} </h5>`);
+        $('#' + 'c'+i).append(`<h2 class="type"> ${gamess[i].type}</h2>`);  
+        $('#' + 'c'+i).append(`<p class="card-text"> ${gamess[i].discription}</p>`);
+        $('#' + 'c'+i).append(`<h2 class="price" >  ${gamess[i].price}</p>`);
